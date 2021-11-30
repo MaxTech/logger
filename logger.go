d8073bbac0e3866ac14bc823db2dd143004e6279
EzMax <47765521+ez-max@users.noreply.github.com>
2021-11-30 18:33:18 +0800

    Update logger.go

--- logger.go
+++ logger.go
@@ -8,15 +8,15 @@ import (
 
 type appLogger struct {
 	loggerName string
-	stdLogger     *log.Logger
-	errLogger     *log.Logger
+	stdLogger  *log.Logger
+	errLogger  *log.Logger
 }
 
 func NewLogger(_loggerName string, _stdWriter, _errWriter io.Writer) *appLogger {
 	return &appLogger{
 		loggerName: _loggerName,
-		stdLogger: log.New(_stdWriter, "", log.LstdFlags),
-		errLogger: log.New(_errWriter, "", log.LstdFlags),
+		stdLogger:  log.New(_stdWriter, "", log.LstdFlags),
+		errLogger:  log.New(_errWriter, "", log.LstdFlags),
 	}
 }
 
@@ -25,73 +25,37 @@ func (al *appLogger) ChangeWriter(_stdWriter, _errWriter io.Writer) {
 	al.errLogger.SetOutput(_errWriter)
 }
 
+func (al appLogger) compose(_flag string) []interface{} {
+	v := []interface{}{
+		fmt.Sprintf("[%s]", _flag),
+	}
+	if len(al.loggerName) > 0 {
+		v = append(v, fmt.Sprintf("[%s]", al.loggerName))
+	}
+	return v
+}
+
 func (al appLogger) Error(v ...interface{}) {
-	v = append([]interface{}{
-		"[ERROR]",
-	}, v...)
-	al.error(v...)
-}
-
-func (al appLogger) Fatal(v ...interface{}) {
-	v = append([]interface{}{
-		"[FATAL]",
-	}, v...)
-	al.fatal(v...)
-}
-
-func (al appLogger) Panic(v ...interface{}) {
-	v = append([]interface{}{
-		"[PANIC]",
-	}, v...)
-	al.panic(v...)
-}
-
-func (al appLogger) Warn(v ...interface{}) {
-	v = append([]interface{}{
-		"[ WARN]",
-	}, v...)
-	al.output(v...)
-}
-
-func (al appLogger) Info(v ...interface{}) {
-	v = append([]interface{}{
-		"[ INFO]",
-	}, v...)
-	al.output(v...)
-}
-
-func (al appLogger) output(v ...interface{}) {
-	if len(al.loggerName) > 0 {
-		v = append([]interface{}{
-			fmt.Sprintf("[%s]", al.loggerName),
-		}, v...)
-	}
-	al.stdLogger.Println(v...)
-}
-
-func (al appLogger) error(v ...interface{}) {
-	if len(al.loggerName) > 0 {
-		v = append([]interface{}{
-			fmt.Sprintf("[%s]", al.loggerName),
-		}, v...)
-	}
+	v = append(al.compose("ERROR"), v...)
 	al.errLogger.Println(v...)
 }
 
-func (al appLogger) fatal(v ...interface{}) {
-	if len(al.loggerName) > 0 {
-		v = append([]interface{}{
-			fmt.Sprintf("[%s]", al.loggerName),
-		}, v...)
-	}
+func (al appLogger) Fatal(v ...interface{}) {
+	v = append(al.compose("FATAL"), v...)
 	al.errLogger.Fatalln(v...)
 }
 
-func (al appLogger) panic(v ...interface{}) {
-	if len(al.loggerName) > 0 {
-		v = append([]interface{}{
-			fmt.Sprintf("[%s]", al.loggerName),
-		}, v...)
-	}
+func (al appLogger) Panic(v ...interface{}) {
+	v = append(al.compose("PANIC"), v...)
 	al.errLogger.Panicln(v...)
+}
+
+func (al appLogger) Warn(v ...interface{}) {
+	v = append(al.compose(" WARN"), v...)
+	al.stdLogger.Println(v...)
+}
+
+func (al appLogger) Info(v ...interface{}) {
+	v = append(al.compose(" INFO"), v...)
+	al.stdLogger.Println(v...)
 }
